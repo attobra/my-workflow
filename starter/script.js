e29af346fc5e4ae33c546290c9f804de63f62074
attobra <nora.attobra@gmail.com>
2021-04-07 09:43:58 -0400

    add 2 steps

--- starter/script.js
+++ starter/script.js
@@ -1,7 +1,8 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
-
+/*
 const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const temperatures2 = [-87, 87];
 
 //1) understanding the problem
 // - what is temp amplitude? difference between highest and lowest temp
@@ -14,15 +15,29 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // -  find min value in temp array
 // - substract min from max to find amplitude and return it
 
-const calcTempAmplitude = function (temps) {
+const calcTempAmplitude = function (temps1, temps2) {
+  const temps = temps1.concat(temps2);
   //find max
   let max = temps[0];
+  let min = temps[0];
   for (let i = 0; i < temps.length; i++) {
-    if (temps[i] > max) {
-      max = temps[i];
-    }
-    console.log(max);
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue; //skip that element
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
   }
+  console.log(min, max);
+  return max - min;
 };
 
-calcTempAmplitude([3, 7, 4, -1, 111]);
+const amplitude = calcTempAmplitude(temperatures, temperatures2);
+console.log(amplitude);
+
+//PROBLEM 2: function should received 2 arrays of temps
+//1) understand the problem
+//with 2 arrays, should we implement functionality 2x? NO just merge the 2 arrays
+
+//2) breaking up into sub-problems
+//how to merge the 2 arrays
+*/
